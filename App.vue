2b2e15cabac6e837747af9a2a69ba829bf0b5623
damingxia <69312774+J-zH0ng@users.noreply.github.com>
2022-06-27 10:47:04 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 引入colorui库 */
+	@import "colorui/main.css";
+	@import "colorui/icon.css";
+	@import "colorui/animation.css";
+	/* 引入图标库 */
+	@import '/static/font/iconfont.css';
+	page {
+		background-color: #fff;
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,30 @@
+import App from './App'
+import store from 'store/index.js'
+// #ifndef VUE3
+import Vue from 'vue'
+
+// 引入uView主JS库
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
+
+import sort from "./components/zwx-sort/zwx-sort"
+Vue.component("sort",sort)
+
+Vue.config.productionTip = false
+App.mpType = 'app'
+const app = new Vue({
+    ...App,
+	store
+})
+app.$mount()
+// #endif
+
+// #ifdef VUE3
+import { createSSRApp } from 'vue'
+export function createApp() {
+  const app = createSSRApp(App)
+  return {
+    app
+  }
+}
+// #endif
